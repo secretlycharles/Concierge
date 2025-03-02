9a1c5b9fabf983b4cbf94c70821c7cae263ba8fc
secretlycharles <imcharlesmendez@gmail.com>
2025-03-02 00:28:19 -0800

    bot base

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from Source.Client.bot import run
+
+
+def main() -> None:
+    """
+    Runs the start function for the discord bot
+
+    :return:
+   """
+
+    # Start bot
+    run()
+
+
+if __name__ == "__main__":
+    main()
